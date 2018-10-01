e40fa82a46f82d8f280012d0e92d320b7c545515
He Zhang <hxzhang1@ualr.edu>
2018-10-01 17:04:12 -0400

    add shell to communicate to remote upboard

---
+++ launch/upboard_comm.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+### according to this webpage http://evakasch.github.io/2016/04/13/ros-comm/
+
+export ROS_MASTER_URI=http://robot:11311
+export ROS_IP="192.168.100.3"
+export ROS_HOSTNAME="davidz-T430"
+
+rosrun rviz rviz -d "vio_laser.rviz"
+
